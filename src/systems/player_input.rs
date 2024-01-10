7cf7eb34a583ffebefc115a7baec173a44de2caa
David Dovel <davey5555@gmail.com>
2024-01-09 18:35:23 -0600

    Transition to using ECS messages to flag movement intent

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,14 +3,13 @@ use legion::world::SubWorld;
 pub use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
+    commands: &mut CommandBuffer,
 ) {
     if let Some(key) = key {
         let delta = match key {
@@ -20,17 +19,20 @@ pub fn player_input(
             VirtualKeyCode::Right => Point::new(1, 0),
             _ => Point::new(0, 0),
         };
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
 
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            });
-        }
+        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        });
+
+        *turn_state = TurnState::PlayerTurn;
     }
 }
